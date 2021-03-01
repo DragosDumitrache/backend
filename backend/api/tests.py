311a1f61d480245a1f0d694966194debd8428f33
Dragos Dumitrache <dragos@afterburner.dev>
2021-03-01 22:39:32 +0000

    Initial action

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
-
 # Create your tests here.
+from .calculations.mathematics import add_two_numbers
+
+
+def test_addition_works():
+    assert add_two_numbers(2, 2) == 4
